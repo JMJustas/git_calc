54b6ce42168935ec4ba11ac086ade5beb655a9b7
Justinas Marcinka <jumar@dbml.dk>
2018-09-17 17:28:34 +0300

    Implement multiplication operation

--- calc.js
+++ calc.js
@@ -12,9 +12,16 @@ const operand2 = parseInt(operand2String);
 
 const add = (a, b) => a + b;
 
+const multiply = function(a, b) {
+  return a * b;
+};
+
 switch (operation) {
   case '+':
     console.log(add(operand1, operand2));
+    break;
+  case '*':
+    console.log(multiply(operand1, operand2));
     break;
   default:
     throw new Error('Unknown operation');
